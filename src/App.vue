79cefdaaf892ecacf83aba6e3530f30b5986119e
Bene-volent <benevolent733@gmail.com>
2023-08-13 12:19:47 +0530

    Corrected the layout

--- src/App.vue
+++ src/App.vue
@@ -81,4 +81,10 @@ import Projects from './components/Projects/Projects.vue';
 body {
   font-family: "Barlow", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
+
+#app{
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+}
 </style>
